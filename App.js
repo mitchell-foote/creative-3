6fe19e7da8c90a1d07e3d1125b61d087cd1dfe38
Mitchell Foote <mitchellfoote93@gmail.com>
2017-03-01 21:23:32 -0700

    Changed up some of the javascript logic, found out that you can either have a seed or have other info, so I made that aparent in the UI

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 ﻿var app = angular.module('app', []);
 app.controller('mainCtrl', mainCtrl);
 
+var baseURL = "https://randomuser.me/api/";
+
 function mainCtrl($scope) {
-    $scope.seed = "";
+    $scope.seed;
     $scope.gender = "male";
     $scope.countryCode = "US";
     $scope.findNewIdent = function () {
-
+        var newURL = baseURL + "?gender=" + $scope.gender.toLowerCase();
+        if ($scope.seed)
+        {
+            newURL += "&seed=" + $scope.seed;
+        }
+        if ($scope.countryCode)
+        {
+            newURL += "&nat=" + $scope.countryCode.toLowerCase();
+        }
+        $.getJSON(newURL, function (data) {
+            var NameInfo = data.results[0].name;
+            var LocationInfo = data.results[0].location;
+            var emailInfo = data.results[0].email;
+            var dob = data.results[0].dob;
+            var phone = data.results[0].phone;
+            var picture = data.results[0].picture.large;
+            var user = new newident(NameInfo, LocationInfo, emailInfo, dob, phone, picture);
+            $scope.ident = user;
+            $scope.$apply();
+        })
     };
     $scope.findRandomIdent = function () {
         $.getJSON(baseURL, function (data) {
@@ -18,13 +39,14 @@ function mainCtrl($scope) {
             var picture = data.results[0].picture.large;
             var user = new newident(NameInfo, LocationInfo, emailInfo, dob, phone, picture);
             $scope.ident = user;
+            $scope.$apply();
         });
     };
-    $scope.ident = {};
+    $scope.ident = new newIdentCurrent();
     $scope.ident.pictureURL = "./Images/default.png";
 }
 
-var baseURL = "https://randomuser.me/api";
+
 var newident = function(nameInfo, locationInfo, emailInfo, dateOfBirth, phoneNumber, pictureURL) {
     var self = this;
     self.name = upperCase(nameInfo.title) + " " + upperCase(nameInfo.first) + " " + upperCase(nameInfo.last);
@@ -37,6 +59,20 @@ var newident = function(nameInfo, locationInfo, emailInfo, dateOfBirth, phoneNum
     self.phone = phoneNumber;
     self.pictureURL = pictureURL;
 };
+
+var newIdentCurrent = function () 
+{
+    var self = this
+    self.name = ""
+    self.street = ""
+    self.city = ""
+    self.state = ""
+    self.postCode = ""
+    self.email = ""
+    self.dateOfBirth = ""
+    self.phone = ""
+    self.pictureURL = "";
+}
 
 var upperCase = function(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
